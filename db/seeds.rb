9aacee757fcaf823153184a9614ebea4e0a1cdf0
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-10-03 21:29:09 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+User.create(:name => 'taneisan', :email => 'taneisan@tanei.com', :profile => '東京都長野区', :image => 'https://hack418b.herokuapp.com/public/img/taneisan.png')
+
+User.create(:name => 'kettsun', :email => 'kettsun@tanei.com', :profile => '兵庫県神戸市', :image => 'https://hack418b.herokuapp.com/public/img/kettsun.png')
+
+User.create(:name => 'moricchi', :email => 'moricchi@tanei.com', :profile => '大阪府堺市', :image => 'https://hack418b.herokuapp.com/public/img/moricchi.png')
+
+User.create(:name => 'ryu-shi', :email => 'ryu-shi@tanei.com', :profile => '兵庫県神戸市', :image => 'https://hack418b.herokuapp.com/public/img/ryu-shi.png')
